f58cee9a2f8a42a14cff6a8a4484d6af3dd2cdfd
spanghf37 <spanghf37@gmail.com>
2021-10-19 17:56:03 +0200

    Update 1-prepare-kvm-host.sh

--- scripts/1-prepare-kvm-host.sh
+++ scripts/1-prepare-kvm-host.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 RESOURCES_DIR=/opt/assisted-installer-resources
 TERRAVERSION=1.0.7 ####change me if needed
+LIBVIRT_HOST_USERNAME=root
+LIBVIRT_HOST_URL=10.0.30.10
 
 dnf -y install $RESOURCES_DIR/epel-release-latest-8.noarch.rpm
 dnf install -y libvirt libvirt-devel qemu-kvm mkisofs python3-devel jq ipmitool  git make bash-completion \
@@ -19,6 +21,9 @@ sed -i 's/#UseDNS yes/UseDNS no/g' /etc/ssh/sshd_config
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 setenforce 0
 ###create kvm pool and network###
+
+ssh-keygen
+ssh-copy-id $LIBVIRT_HOST_USERNAME@$LIBVIRT_HOST_URL
 
 cp -r ../terraform /opt/
 
