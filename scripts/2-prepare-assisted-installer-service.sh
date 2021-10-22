44ee6f1bdf83c9e659f111ef754a6ee6d62b6ee7
spanghf37 <spanghf37@gmail.com>
2021-10-22 10:28:09 +0200

    Update 2-prepare-assisted-installer-service.sh

--- scripts/2-prepare-assisted-installer-service.sh
+++ scripts/2-prepare-assisted-installer-service.sh
@@ -27,6 +27,9 @@ sed -i 's/PUBLIC_CONTAINER_REGISTRIES=.*/PUBLIC_CONTAINER_REGISTRIES='"$IP"':501
 echo 'AGENT_DOCKER_IMAGE='"$IP"':5015/ocpmetal/assisted-installer-agent:latest' >> onprem-environment
 echo "SKIP_CERT_VERIFICATION=true" >> onprem-environment
 
+## Modify agent.service inside RHCOS LIVE ISO to pull from local registry
+sed -i 's,ExecStartPre=/usr/local/bin/agent-fix-bz1964591 {{.AgentDockerImg}}\\n,ExecStartPre=podman pull --tls-verify=false '"$IP"':5015/ocpmetal/assisted-installer-agent:latest\\nExecStartPre=/usr/local/bin/agent-fix-bz1964591 {{.AgentDockerImg}}\\n,' /$RESOURCES_DIR/assisted-service/internal/ignition/ignition.go
+
 sed -i "s/5432,8000,8090,8080/5432:5432 -p 8000:8000 -p 8090:8090 -p 8080:8080/" Makefile
 make deploy-onprem
 podman ps
