3829dd5f88ade7de092aeb03e5e03cc6443feaa3
spanghf37 <spanghf37@gmail.com>
2021-11-02 16:45:43 +0100

    Update 2-prepare-assisted-installer-service.sh

--- scripts/2-prepare-assisted-installer-service.sh
+++ scripts/2-prepare-assisted-installer-service.sh
@@ -3,6 +3,7 @@
 #!/bin/bash
 
 RESOURCES_DIR=/opt/assisted-service-resources
+PULL_SECRET_UPDATE=/home/pull-secret-update.txt
 
 ##Cleaning
 podman pod stop assisted-installer
@@ -21,14 +22,21 @@ IP="${host_fqdn}"
 
 AI_URL=http://$IP:8090
 
+## Update agent container image (/ocpmetal/assisted-installer-agent:latest) with local registry CRT (for oc adm release extract to work)
+podman login $IP:5015 --authfile $PULL_SECRET_UPDATE
+cat <<EOT > $RESOURCES_DIR/Dockerfile-assisted-installer-agent
+FROM $IP:5015/ocpmetal/assisted-installer-agent:latest
+ADD /registry/certs/domain.crt /etc/pki/ca-trust/source/anchors/registry.crt
+RUN chmod 644 /etc/pki/ca-trust/source/anchors/registry.crt && update-ca-trust extract
+EOT
+podman build --file $RESOURCES_DIR/Dockerfile-assisted-installer-agent --authfile $PULL_SECRET_UPDATE -t $IP:5015/ocpmetal/assisted-installer-agent:latest-custom-crt
+podman push $IP:5015/ocpmetal/assisted-installer-agent:latest-custom-crt --authfile $PULL_SECRET_UPDATE
+
 ## Modify onprem-environment and Makefile to set proper URL and port forwarding
 sed -i "s@SERVICE_BASE_URL=.*@SERVICE_BASE_URL=$AI_URL@" onprem-environment
 sed -i 's/PUBLIC_CONTAINER_REGISTRIES=.*/PUBLIC_CONTAINER_REGISTRIES='"$IP"':5015/' onprem-environment
-echo 'AGENT_DOCKER_IMAGE='"$IP"':5015/ocpmetal/assisted-installer-agent:latest' >> onprem-environment
+echo 'AGENT_DOCKER_IMAGE='"$IP"':5015/ocpmetal/assisted-installer-agent:latest-custom-crt' >> onprem-environment
 echo "SKIP_CERT_VERIFICATION=true" >> onprem-environment
-
-## Modify agent.service inside RHCOS LIVE ISO to pull from local registry
-## sed -i 's,ExecStartPre=/usr/local/bin/agent-fix-bz1964591 {{.AgentDockerImg}}\\n,ExecStartPre=podman pull --tls-verify=false '"$IP"':5015/ocpmetal/assisted-installer-agent:latest\\nExecStartPre=/usr/local/bin/agent-fix-bz1964591 {{.AgentDockerImg}}\\n,' /$RESOURCES_DIR/assisted-service/internal/ignition/ignition.go
 
 sed -i "s/5432,8000,8090,8080/5432:5432 -p 8000:8000 -p 8090:8090 -p 8080:8080/" Makefile
 make deploy-onprem
